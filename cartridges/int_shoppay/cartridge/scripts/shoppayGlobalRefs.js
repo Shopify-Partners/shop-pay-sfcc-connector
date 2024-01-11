929ca6b36d8762d4b2832cf3214690943d030f8b
Aidrian O'Connor <public@unitone.org>
2024-01-11 11:57:27 -0500

    SSPSC-17: creates initial global references script

--- cartridges/int_shoppay/cartridge/scripts/shoppayGlobalRefs.js
+++ cartridges/int_shoppay/cartridge/scripts/shoppayGlobalRefs.js
@@ -11,14 +11,67 @@ var urls = {
     SubmitPayment: URLUtils.https('ShopPay-SubmitPayment').toString()
 };
 
-// aidrian TODO: fine to just check if enabled, or do we want to
-// use PaymentMgr.getApplicablePaymentMethods here?
+// core reference for if Shop Pay is enabled, controlled by
+// enabling or disabling the Shop Pay payment method
 var shoppayEnabled = function() {
     var paymentMethod = PaymentMgr.getPaymentMethod('ShopPay');
     return (paymentMethod) ? paymentMethod.isActive() : false;
 };
 
-module.exports = {
+// Kristin TODO: flesh this logic out
+// core reference for if the Shop Pay payment method is valid based on
+// payment amount, country / regions, customer group, etc
+function shoppayApplicable() { // req, currentBasket
+    // var paymentAmount = currentBasket.totalGrossPrice.value;
+    // var countryCode = req.geolocation.countryCode;
+    // var currentCustomer = req.currentCustomer.raw;
+    // return PaymentMgr.getApplicablePaymentMethods(
+    //     currentCustomer,
+    //     countryCode,
+    //     paymentAmount
+    // );
+    return true;
+}
+
+// abstracted helper to get any site pref value by ID
+// will return false if preference is not found, or if
+// pref value type is boolean and is set to either None or No
+function getSitePref(id) {
+    var pref = dw.system.Site.current.getCustomPreferenceValue(id);
+    return (pref) ? pref : false;
+}
+// shortcut references to individual Shop Pay site preference values
+var isShoppayPDPButtonEnabled   = function() { return getSitePref('shoppayPDPButtonEnabled'); }
+var isShoppayCartButtonEnabled  = function() { return getSitePref('shoppayCartButtonEnabled'); }
+var shoppayStoreId              = function() { return getSitePref('shoppayStoreId'); }
+var shoppayAdminAPIVersion      = function() { return getSitePref('shoppayAdminAPIVersion'); }
+var shoppayStorefrontAPIVersion = function() { return getSitePref('shoppayStorefrontAPIVersion'); }
+
+
+/*
+ add any values you want made available to client-side JS to the object below.
+ then, in controller, add:
+    res.viewData.shoppayClientRefs = JSON.stringify(shoppayGlobalRefs.clientRefs);
+ then, in ISML add:
+    <script>
+        window.shoppayClientRefs = JSON.parse('<isprint encoding="jsonvalue" value="${pdict.shoppayClientRefs}" />');
+    </script>
+*/
+var clientRefs = {
     urls: urls,
-    shoppayEnabled: shoppayEnabled
+    constants: {
+        shoppayEnabled: shoppayEnabled()
+    }
+}
+
+
+module.exports = {
+    shoppayEnabled: shoppayEnabled,
+    shoppayApplicable: shoppayApplicable,
+    isShoppayPDPButtonEnabled: isShoppayPDPButtonEnabled(),
+    isShoppayCartButtonEnabled: isShoppayCartButtonEnabled(),
+    shoppayStoreId: shoppayStoreId(),
+    shoppayAdminAPIVersion: shoppayAdminAPIVersion(),
+    shoppayStorefrontAPIVersion: shoppayStorefrontAPIVersion(),
+    clientRefs: clientRefs
 };
--- cartridges/int_shoppay_sfra/cartridge/controllers/Product.js
+++ cartridges/int_shoppay_sfra/cartridge/controllers/Product.js
@@ -13,10 +13,8 @@ server.extend(page);
  * Kristin TODO: Build out and reference conditional logic helper script to set the value of includeShopPayJS
  */
 server.append('Show', csrfProtection.generateToken, function (req, res, next) {
-    res.viewData.includeShopPayJS = true;
-    var shoppayEnabled = shoppayGlobalRefs.shoppayEnabled();
-    var shoppayUrls = shoppayGlobalRefs.urls; // note this is a keyed object
-    var SPGetCartSummaryURL = shoppayGlobalRefs.urls.GetCartSummary;
+    res.viewData.includeShopPayJS = shoppayGlobalRefs.shoppayApplicable();
+    res.viewData.shoppayClientRefs = JSON.stringify(shoppayGlobalRefs.clientRefs);
     next();
 });
 
