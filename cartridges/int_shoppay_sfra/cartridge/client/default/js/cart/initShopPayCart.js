9a5ae8d47490965a53284edb659233d2e99d8401
benjaminMaze <122311112+benjaminMaze@users.noreply.github.com>
2024-01-22 12:50:26 -0500

    FE Session Init (#14)
    * FE Session Init
    
    * Code Review - Renaming Client Side helper file

--- cartridges/int_shoppay_sfra/cartridge/client/default/js/cart/initShopPayCart.js
+++ cartridges/int_shoppay_sfra/cartridge/client/default/js/cart/initShopPayCart.js
@@ -1,10 +1,14 @@
+const helper = require('../helpers/shoppayHelper');
+
 $(document).ready(function () {
     if(window.ShopPay) {
         if(window.shoppayClientRefs.constants.initShopPayEmailRecognition) {
             initShopPayEmailRecognition();
         }
-        
+
         initShopPayButton();
+
+        const session = initShopPaySession();
     }
 });
 
@@ -31,4 +35,20 @@ function initShopPayEmailRecognition() {
     */
     window.ShopPay.PaymentRequest.createLogin({emailInputId: 'email-guest'})
         .render('#shop-pay-login-container');
+}
+
+function initShopPaySession() {
+    const paymentRequestResponse = $.ajax({
+        url: helper.getUrlWithCsrfToken(window.shoppayClientRefs.urls.GetCartSummary),
+        type: 'GET',
+        contentType: 'application/json',
+        async: false
+    }) || {};
+
+    var paymentRequest = paymentRequestResponse.responseJSON.paymentRequest;
+    const initialPaymentRequest = window.ShopPay.PaymentRequest.build(paymentRequest);
+
+    return window.ShopPay.PaymentRequest.createSession({
+        paymentRequest: initialPaymentRequest
+    });
 }
