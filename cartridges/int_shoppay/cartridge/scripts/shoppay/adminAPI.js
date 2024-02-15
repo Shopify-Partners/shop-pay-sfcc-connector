06106ac099ae4e330161b4b2afa62d8a1a9b716f
kvanandel-maze <116684733+kvanandel-maze@users.noreply.github.com>
2024-02-15 07:48:11 -0600

    sspsc-48: webhook subscription jobs (#32)

--- cartridges/int_shoppay/cartridge/scripts/shoppay/adminAPI.js
+++ cartridges/int_shoppay/cartridge/scripts/shoppay/adminAPI.js
@@ -2,6 +2,10 @@
 
 var logger = require('dw/system/Logger').getLogger('ShopPay', 'ShopPay');
 
+const webhookIncludeFields = {
+    "ORDERS_CREATE": ["admin_graphql_api_id","name","source_identifier","email","billing_address","total_price","confirmed","customer"]
+};
+
 /**
  * Function to retrieve the details of a Shop Pay order using the Shopify GraphQL Admin API
  * @param {string} sourceIdentifier - the sourceIdentifier value (SFCC basket UUID) sent to Shopify during checkout
@@ -45,6 +49,130 @@ function getOrderBySourceIdentifier(sourceIdentifier) {
     }
 }
 
+/**
+ * Subscribes an SFCC controller endpoint to a relevant Shopify webhook.
+ * @param {string} topic - The webhook topic
+ * @param {string} callbackUrl - The webhook callback URL
+ * @returns {Object} - An object representing an error or the GraphQL service response body
+ */
+function subscribeWebhook(topic, callbackUrl) {
+    var queryString = "mutation webhookSubscriptionCreate($topic: WebhookSubscriptionTopic!, $webhookSubscription: WebhookSubscriptionInput!) {webhookSubscriptionCreate(topic: $topic, webhookSubscription: $webhookSubscription) {userErrors {field message} webhookSubscription {id legacyResourceId format topic endpoint {__typename ... on WebhookHttpEndpoint {callbackUrl}}}}}";
+    const bodyObj = {
+        query: queryString,
+        variables: {
+            topic: topic,
+            webhookSubscription: {
+                format: "JSON",
+                callbackUrl: callbackUrl
+            }
+        }
+    };
+    if (webhookIncludeFields[topic]) {
+        bodyObj.variables.webhookSubscription.includeFields = webhookIncludeFields[topic];
+    }
+
+    var shoppayAdminService = require('*/cartridge/scripts/shoppay/service/shoppayAdminService')();
+    var response = shoppayAdminService.call({
+        body: bodyObj || {}
+    });
+
+    var responseHeaders = shoppayAdminService.client.responseHeaders;
+    var shopifyRequestID = responseHeaders.get('X-Request-ID');
+    if (shopifyRequestID && shopifyRequestID.length > 0) {
+        logger.info('X-Request-ID: {0}', shopifyRequestID[0]);
+    }
+
+    if (!response.ok
+        || !response.object
+        || !response.object.data
+        || (response.object.errors && response.object.errors.length > 0)
+    ) {
+        return {
+            error: true,
+            errorMsg: response.errorMessage
+        };
+    }
+    return response.object.data;
+}
+
+/**
+ * Searches for an existing Shopify webhook subscription with matching topic and callback Url.
+ * @param {string} topic - The webhook topic
+ * @param {string} callbackUrl - The webhook callback URL (SFCC controller endpoint as absolute URL)
+ * @returns {Object} - An object representing an error or the GraphQL service response body
+ */
+function getExistingWebhook(topic, callbackUrl) {
+    var queryString = "{webhookSubscriptions(first: 1, topics:" + topic + ", callbackUrl: \"" + callbackUrl + "\") {edges {node {id legacyResourceId format topic endpoint {__typename ... on WebhookHttpEndpoint {callbackUrl}}}}}}";
+    const bodyObj = {
+        query: queryString,
+        variables: {}
+    };
+
+    var shoppayAdminService = require('*/cartridge/scripts/shoppay/service/shoppayAdminService')();
+    var response = shoppayAdminService.call({
+        body: bodyObj || {}
+    });
+
+    var responseHeaders = shoppayAdminService.client.responseHeaders;
+    var shopifyRequestID = responseHeaders.get('X-Request-ID');
+    if (shopifyRequestID && shopifyRequestID.length > 0) {
+        logger.info('X-Request-ID: {0}', shopifyRequestID[0]);
+    }
+
+    if (!response.ok
+        || !response.object
+        || !response.object.data
+        || (response.object.errors && response.object.errors.length > 0)
+    ) {
+        return {
+            error: true,
+            errorMsg: response.errorMessage
+        };
+    }
+    return response.object.data;
+}
+
+/**
+ * Unsubscribes an SFCC controller endpoint from a Shopify webhook.
+ * @param {string} id - The webhook ID to unsubscribe (format: "gid://shopify/WebhookSubscription/1464424628544")
+ * @returns {Object} - An object representing an error or the GraphQL service response body
+ */
+function unsubscribeWebhook(id) {
+    var queryString = "mutation webhookSubscriptionDelete($id: ID!) {webhookSubscriptionDelete(id: $id) {deletedWebhookSubscriptionId userErrors {field message }}}";
+    const bodyObj = {
+        query: queryString,
+        variables: {
+            id: id
+        }
+    };
+
+    var shoppayAdminService = require('*/cartridge/scripts/shoppay/service/shoppayAdminService')();
+    var response = shoppayAdminService.call({
+        body: bodyObj || {}
+    });
+
+    var responseHeaders = shoppayAdminService.client.responseHeaders;
+    var shopifyRequestID = responseHeaders.get('X-Request-ID');
+    if (shopifyRequestID && shopifyRequestID.length > 0) {
+        logger.info('X-Request-ID: {0}', shopifyRequestID[0]);
+    }
+
+    if (!response.ok
+        || !response.object
+        || !response.object.data
+        || (response.object.errors && response.object.errors.length > 0)
+    ) {
+        return {
+            error: true,
+            errorMsg: response.errorMessage
+        };
+    }
+    return response.object.data;
+}
+
 module.exports = {
-    getOrderBySourceIdentifier: getOrderBySourceIdentifier
+    getOrderBySourceIdentifier: getOrderBySourceIdentifier,
+    subscribeWebhook: subscribeWebhook,
+    getExistingWebhook: getExistingWebhook,
+    unsubscribeWebhook: unsubscribeWebhook
 };
