482fe091185bbe194f39f05bb602129866188f60
kvanandel-maze <116684733+kvanandel-maze@users.noreply.github.com>
2024-02-28 09:45:32 -0600

    sspsc-31: orders create webhook handler (#47)

--- cartridges/int_shoppay/cartridge/scripts/shoppay/jobsteps/OrderReconciliation.js
+++ cartridges/int_shoppay/cartridge/scripts/shoppay/jobsteps/OrderReconciliation.js
@@ -22,7 +22,7 @@ function processOrder(order) {
     var sourceIdentifier = order.custom.shoppaySourceIdentifier;
     var response = adminAPI.getOrderBySourceIdentifier(sourceIdentifier);
     if (response.error || response.orders.edges.length == 0) {
-        logger.error('Shopify order not found for SFCC order ' + order.orderNo);
+        logger.error('[OrderReconciliation.js] Shopify order not found for SFCC order ' + order.orderNo);
         // This order will be reattempted on next run
         return;
     }
@@ -32,7 +32,7 @@ function processOrder(order) {
     postProcessingHelpers.handleBillingInfo(order, node);
     var placeOrderResult = postProcessingHelpers.placeOrder(order);
     if (placeOrderResult.error) {
-        logger.error('Unable to place order ' + order.orderNo);
+        logger.error('[OrderReconciliation.js] Unable to place order ' + order.orderNo);
         // This order will be reattempted on next run
         return;
     }
@@ -53,19 +53,19 @@ exports.Run = function(params, stepExecution) {
         var min = new Date(nowMillis - minOrderAgeSecs * 1000).toISOString(); // seconds to milliseconds
         if (maxOrderAgeHrs) {
             var max = new Date(nowMillis - maxOrderAgeHrs * 60 * 60 * 1000).toISOString(); // hours to milliseconds
-            queryString = 'creationDate >= {0} AND creationDate <= {1} AND status = {2} AND custom.shoppayOrder = {3} AND custom.shoppayOrderCreateWebhookReceived != {4}';
-            OrderMgr.processOrders(processOrder, queryString, max, min, Order.ORDER_STATUS_CREATED, true, true);
+            queryString = 'creationDate >= {0} AND creationDate <= {1} AND status = {2} AND custom.shoppayOrder = {3}';
+            OrderMgr.processOrders(processOrder, queryString, max, min, Order.ORDER_STATUS_CREATED, true);
         } else {
-            queryString = 'creationDate <= {0} AND status = {1} AND custom.shoppayOrder = {2} AND custom.shoppayOrderCreateWebhookReceived != {3}';
-            OrderMgr.processOrders(processOrder, queryString, min, Order.ORDER_STATUS_CREATED, true,  true);
+            queryString = 'creationDate <= {0} AND status = {1} AND custom.shoppayOrder = {2}';
+            OrderMgr.processOrders(processOrder, queryString, min, Order.ORDER_STATUS_CREATED, true);
         }
     } catch (e) {
         if (orders) {
             orders.close();
         }
         logger.error('[OrderReconciliation.js] error: \n\r' + e.message + '\n\r' + e.stack);
-        return new Status(Status.ERROR, null, 'Exception thrown: ' + e.message);
+        return new Status(Status.ERROR, 'ERROR', 'Exception thrown: ' + e.message);
     }
 
-    return new Status(Status.OK, null, 'Successfully processed {0} / {1} orders', successCount, orderCount);
+    return new Status(Status.OK, 'OK', 'Successfully processed {0} / {1} orders', successCount, orderCount);
 };
