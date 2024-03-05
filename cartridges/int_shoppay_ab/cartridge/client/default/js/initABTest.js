458827438878170940e6240cc9d7ee61fb259b51
benjaminMaze <122311112+benjaminMaze@users.noreply.github.com>
2024-03-05 13:21:43 -0500

    [SSPSC-69] fixing udefined property value (#56)

--- cartridges/int_shoppay_ab/cartridge/client/default/js/initABTest.js
+++ cartridges/int_shoppay_ab/cartridge/client/default/js/initABTest.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
                     subjectId: subjectId,
                     assignmentGroup: assignmentGroup,
                     shopId: window.shoppayClientRefs.preferences.shoppayStoreId,
-                    experimentId: window.shoppayClientRefs.preferences.shoppayExperimentId,
+                    experimentId: window.shoppayClientRefs.constants.shoppayExperimentId,
                     timestamp: new Date().toISOString()
                 });
                 abCookieJSON['isAssigned'] = true;
