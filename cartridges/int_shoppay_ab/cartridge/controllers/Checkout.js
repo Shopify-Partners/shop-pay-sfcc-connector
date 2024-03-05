ec50dc7d664270113ec5e0d101a407124a84f0dc
Benjamin Penton <benjamin@themazegroup.com>
2024-03-05 15:19:52 -0500

    SSPSC-71 fixing checkout-complete events not firing

--- cartridges/int_shoppay_ab/cartridge/controllers/Checkout.js
+++ cartridges/int_shoppay_ab/cartridge/controllers/Checkout.js
@@ -43,7 +43,8 @@ server.append('Begin', csrfProtection.generateToken, function (req, res, next) {
         activeAssignmentGroup = shoppayABCookieValue.assignmentGroup;
     }
 
-    viewData.includeShopPayJS = activeABTest !== 'shoppayAA' && activeAssignmentGroup === 'treatment';
+    //The ShopPay SDK should alaways be added for A/A or A/B test so we can send tracking events
+    viewData.includeShopPayJS = true;
     var shoppayClientRefs = JSON.parse(viewData.shoppayClientRefs);
     var shoppayExperimentId = currentSite.getCustomPreferenceValue('shoppayExperimentId');
     if(shoppayExperimentId) {
