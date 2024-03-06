07dd1bcf4c3479f0c666c2cbfb74ffc038503d30
benjaminMaze <122311112+benjaminMaze@users.noreply.github.com>
2024-03-06 10:55:13 -0500

    SSPSC-71 fixing checkout-complete events not firing (#57)
    Co-authored-by: Kristin Van Andel <kristin@themazegroup.com>

--- cartridges/int_shoppay_ab/cartridge/controllers/Checkout.js
+++ cartridges/int_shoppay_ab/cartridge/controllers/Checkout.js
@@ -43,7 +43,8 @@ server.append('Begin', csrfProtection.generateToken, function (req, res, next) {
         activeAssignmentGroup = shoppayABCookieValue.assignmentGroup;
     }
 
-    viewData.includeShopPayJS = activeABTest !== 'shoppayAA' && activeAssignmentGroup === 'treatment';
+    //The ShopPay SDK should alaways be added for A/A or A/B test so we can send tracking events
+    viewData.includeShopPayJS = true;
     var shoppayClientRefs = JSON.parse(viewData.shoppayClientRefs);
     var shoppayExperimentId = currentSite.getCustomPreferenceValue('shoppayExperimentId');
     if(shoppayExperimentId) {
@@ -51,6 +52,11 @@ server.append('Begin', csrfProtection.generateToken, function (req, res, next) {
         viewData.shoppayClientRefs = viewData.includeShopPayJS
             ? JSON.stringify(shoppayClientRefs)
             : JSON.stringify({});
+    }
+
+    if(activeABTest === 'shoppayAA' || activeAssignmentGroup === 'control') {
+        shoppayClientRefs['preferences']['shoppayAATest'] = true;
+        viewData.hideCheckoutShoppayButton = true;
     }
 
     viewData.initShopPayABTest = true;
