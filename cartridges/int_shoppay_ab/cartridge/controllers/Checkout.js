94c1c77d73776d86a84966b16a57c2502a9c941d
benjaminMaze <122311112+benjaminMaze@users.noreply.github.com>
2024-03-12 11:20:10 -0400

    Fix/sspsc 71 checkout complete events not firing (#61)
    Co-authored-by: Kristin Van Andel <kristin@themazegroup.com>

--- cartridges/int_shoppay_ab/cartridge/controllers/Checkout.js
+++ cartridges/int_shoppay_ab/cartridge/controllers/Checkout.js
@@ -60,11 +60,6 @@ server.append('Begin', csrfProtection.generateToken, function (req, res, next) {
             ? JSON.stringify(shoppayClientRefs)
             : JSON.stringify({});
 
-    if(activeABTest === 'shoppayAA' || activeAssignmentGroup === 'control') {
-        shoppayClientRefs['preferences']['shoppayAATest'] = true;
-        viewData.hideCheckoutShoppayButton = true;
-    }
-
     viewData.initShopPayABTest = true;
     res.setViewData(viewData);
 
